87c1babb55de48f7dfa3bfa0900391645277eb1e
yamboni1 <ncyambot.work@gmail.com>
2023-08-17 19:11:46 +0800

    test: added test when given command is to move and direction is south

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -18,4 +18,18 @@ class MarsRoverTest {
         Assertions.assertEquals(1 , currentLocation.getY());
         Assertions.assertEquals(Direction.NORTH , currentLocation.getDirection());
     }
+    @Test
+    void should_change_to_location_0_negative1_S_when_executeCommand_given_0_0_South_and_command_Move() {
+        // Given
+        Location initialLocation = new Location(0, 0, Direction.SOUTH);
+        Command givenCommand = Command.MOVE;
+        MarsRover marsRover = new MarsRover(initialLocation);
+        // When
+        marsRover.executeCommand(givenCommand);
+        Location currentLocation = marsRover.getCurrentLocation();
+        // Then
+        Assertions.assertEquals(0 , currentLocation.getX());
+        Assertions.assertEquals(-1 , currentLocation.getY());
+        Assertions.assertEquals(Direction.SOUTH , currentLocation.getDirection());
+    }
 }

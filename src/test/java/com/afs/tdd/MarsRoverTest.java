bba58c5434479feb3d3162e6a32595f2735fe5b6
yamboni1 <ncyambot.work@gmail.com>
2023-08-17 20:06:45 +0800

    test: add test case when given command is turn left with current location 0 0 S and expected output is 0 0 E

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -74,6 +74,20 @@ class MarsRoverTest {
         Assertions.assertEquals( 0 , currentLocation.getY());
         Assertions.assertEquals(Direction.WEST , currentLocation.getDirection());
     }
+    @Test
+    void should_change_to_direction_0_0_East_when_executeCommand_given_0_0_South_and_command_Turn_Left() {
+        // Given
+        Location initialLocation = new Location(0, 0, Direction.SOUTH);
+        Command givenCommand = Command.TURN_LEFT;
+        MarsRover marsRover = new MarsRover(initialLocation);
+        // When
+        marsRover.executeCommand(givenCommand);
+        Location currentLocation = marsRover.getCurrentLocation();
+        // Then
+        Assertions.assertEquals(0 , currentLocation.getX());
+        Assertions.assertEquals( 0 , currentLocation.getY());
+        Assertions.assertEquals(Direction.EAST , currentLocation.getDirection());
+    }
 
 
 }

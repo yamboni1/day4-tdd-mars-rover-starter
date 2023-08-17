b9a3863f1234c07b13fb5b7880a9ba71d5d67400
yamboni1 <ncyambot.work@gmail.com>
2023-08-17 19:34:35 +0800

    test: add test case when given command is move and direction is West

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -46,4 +46,19 @@ class MarsRoverTest {
         Assertions.assertEquals( 0 , currentLocation.getY());
         Assertions.assertEquals(Direction.EAST , currentLocation.getDirection());
     }
+    @Test
+    void should_change_to_location_negative1_0_W_when_executeCommand_given_0_0_West_and_command_Move() {
+        // Given
+        Location initialLocation = new Location(0, 0, Direction.WEST);
+        Command givenCommand = Command.MOVE;
+        MarsRover marsRover = new MarsRover(initialLocation);
+        // When
+        marsRover.executeCommand(givenCommand);
+        Location currentLocation = marsRover.getCurrentLocation();
+        // Then
+        Assertions.assertEquals(-1 , currentLocation.getX());
+        Assertions.assertEquals( 0 , currentLocation.getY());
+        Assertions.assertEquals(Direction.WEST , currentLocation.getDirection());
+    }
+
 }
